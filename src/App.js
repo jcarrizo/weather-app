5dda2df670c358627e3c9555663ae69418b0ea92
Jesus Carrizo <jcarrizo15@gmail.com>
2021-09-06 21:18:03 -0300

    Primero cambios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import Search from "./Components/Search/Search";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Search />
       </header>
     </div>
   );
---
+++ src/Components/Search/Search.js
@@ -0,0 +1,6 @@
+import "./search.css";
+
+function Search() {
+  return <div></div>;
+}
+export default Search;
